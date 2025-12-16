8e9443f21d708470ce388d31499c24f4ffb46671
nomad <navarifaya@gmail.com>
2025-12-16 16:53:41 +0530

    setting up UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Tasks from "./pages/Tasks";
 
 function App() {
-  return <h1>TodoApp Frontend</h1>;
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/tasks"
+          element={isAuthenticated ? <Tasks /> : <Navigate to="/login" />}
+        />
+        <Route path="*" element={<Navigate to="/tasks" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
